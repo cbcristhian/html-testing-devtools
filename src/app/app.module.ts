3edeb72ab6b7dfc31146d27e17f81c5f6b1a591b
cbcristhian <cristhian.castro@unosquare.com>
2023-03-17 09:51:38 -0400

    conflicts resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { AboutComponent } from './components/about/about.component';
     FooterComponent,
     IntroComponent,
     AboutComponent,
+    ContactComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
